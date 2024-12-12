9d8e59ab53ce0e7f081887ec8d90909710083b6f
Ariel Angelo <ariel.costalves@gmail.com>
2024-12-12 15:53:39 -0300

    Refatorar importações e formatar rotas no componente App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
 import Footer from "./components/Footer";
 import ListaProf from "./components/ListaProf";
 import Calendario from "./components/Calendario";
-import ListaUBS from "./components/ListaUBS";
+import ListaUBS from "./lista-ubs/src/components/ListaUBS";
 import CreateProfessional from "./components/CreateProfessional";
 import CreateCampaign from "./components/CreateCampaign";
 
@@ -24,10 +29,22 @@ function App() {
           <Routes>
             <Route path="/" element={<ListaUBS />} />
             <Route path="/login" element={<LoginForm />} />
-            <Route path="/listar" element={<PrivateRoute element={ListaProf} />} />
-            <Route path="/calendario" element={<PrivateRoute element={Calendario} />} />
-            <Route path="/createCampaign" element={<PrivateRoute element={CreateCampaign} />} />
-            <Route path="/createProfessional" element={<PrivateRoute element={CreateProfessional} />} />
+            <Route
+              path="/listar"
+              element={<PrivateRoute element={ListaProf} />}
+            />
+            <Route
+              path="/calendario"
+              element={<PrivateRoute element={Calendario} />}
+            />
+            <Route
+              path="/createCampaign"
+              element={<PrivateRoute element={CreateCampaign} />}
+            />
+            <Route
+              path="/createProfessional"
+              element={<PrivateRoute element={CreateProfessional} />}
+            />
           </Routes>
         </main>
         <Footer />
